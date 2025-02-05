7e5f600166b3cf1db42269fd566571261d510a5d
Poe Jones <poejones@mxf.mozmail.com>
2025-02-05 10:37:28 -0600

    remove blog

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/gregg/github/cabanawiki/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/gregg/github/cabanawiki/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/gregg/github/cabanawiki/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/gregg/github/cabanawiki/src/css/custom.css"),
+];
